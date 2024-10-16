c9a88d947e14da18d4d26b7f7441facc68143b66
darieny <darienycarpio@gmail.com>
2024-10-15 21:59:56 -0600

    Cambio en bdjs

--- src/models/db.js
+++ src/models/db.js
@@ -3,40 +3,35 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-// Configuración de la conexión
-const connection = createConnection({
+let connection;
+
+function handleDisconnect() {
+  connection = createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     port: process.env.DB_PORT
-});
-// Intento de conexión con manejo de errores
-connection.connect((err) => {
+  });
+// Intentar conectarse
+  connection.connect(err => {
     if (err) {
-        console.error('Error al conectar a la base de datos:', err);
-        return;
+      console.error('Error al intentar conectarse a la base de datos:', err);
+      setTimeout(handleDisconnect, 2000);  // Intentar reconectar después de 2 segundos
+    } else {
+      console.log('Conexión a la base de datos establecida');
     }
-    console.log(`Conexión exitosa a la base de datos: ${process.env.DB_DATABASE}`);
-
-    // Verificación de la base de datos seleccionada
-    connection.query('SELECT DATABASE()', (err, result) => {
-        if (err) {
-            console.error('Error al obtener la base de datos actual:', err);
-        } else {
-            console.log('Base de datos actual:', result[0]['DATABASE()']);
+  });
+// Manejar desconexiones inesperadas
+  connection.on('error', err => {
+    console.error('Error en la conexión a la base de datos:', err);
+    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+      handleDisconnect();  // Reconectar automáticamente en caso de pérdida de conexión
+    } else {
+      throw err;
+    }
+  });
 }
-    });
-
-    connection.query('SHOW TABLES', (err, results) => {
-        if (err) {
-            console.error('Error en la consulta SHOW TABLES:', err);
-            return;
-        }
-        console.log('Tablas disponibles:', results);
-    });
-});
-
-
+handleDisconnect();  // Iniciar la conexión
 
 export default connection;
